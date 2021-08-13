7d9df6cab36f603c57e57f1ece76282735b0d61f
Lara Klemme <laraklemme@gmail.com>
2021-08-13 16:42:42 +0200

    links

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,11 +12,17 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(name: params['list']['name'])
+    @list = List.new(list_params)
     if @list.save
       redirect_to list_path(@list)
     else
       render :new
     end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
   end
 end
